aa992791e83907b539601885813c5ab5908466f7
Yulia Alekseenko <Yulia_Alekseenko@epam.com>
2018-09-12 14:16:08 +0300

    add session support

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -1,4 +1,17 @@
 package servlets;
 
-public class LoginServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+        session.setAttribute("authorized", new Boolean(true));
+        response.sendRedirect("/table");
+    }
 }
